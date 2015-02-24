9ee945d00899d845a531ac23f14a031c8b4a2419
naeem <naeem.itsrelevant@nxvt.com>
2015-02-24 20:17:36 +0500

    configured express

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-var http = require('http');
 var port = 1337;
- 
-http.createServer(function(req, res) {
-      res.writeHead(200, {
-            'Content-Type': 'text/plain'
-      });
- 
-      res.end("Hello Mitnick, you've just stumbled on the simplest web server ever");
-}).listen(port);
- 
-console.log('Our awesome web server running at http://localhost:'+ port);
+var express = require('./config/express');
+
+var app = express();
+
+app.listen(port);
+module.exports = app;
+
